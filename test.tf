4b4a9629fc8f743a8e6671c804c80f85b3115f23
Sukhbirsingh Khalsa <sukhbirkhalsait@gmail.com>
2026-02-05 14:11:50 +0530

    Create test.tf

---
+++ test.tf
@@ -0,0 +1,8 @@
+provider {
+features {}
+subscription_id: 331a949f-b38b-4711-a7fb-64ce9e156260
+}
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "West Europe"
+}
