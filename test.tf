59eb41293a0949652455572957f4f5c7651ee1d2
Sukhbirsingh Khalsa <sukhbirkhalsait@gmail.com>
2026-02-05 14:24:13 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,6 +1,6 @@
 provider {
 features {}
-subscription_id: 331a949f-b38b-4711-a7fb-64ce9e156260
+subscription_id = "331a949f-b38b-4711-a7fb-64ce9e156260"
 }
 resource "azurerm_resource_group" "example" {
   name     = "example"
